ed4b1b113f2c13542f85dc61b1650dd2e171751b
Shakes M <smkhonza@gmail.com>
2016-04-12 13:29:49 +0200

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = new DateTime(2016,02, 13);
+            DateTime date2 = date.GetValueOrDefault();
+
+            // Null coalescing operator
+            // if date3 is null then set date3 = date else set date3 = datetime.today
+            DateTime date3 = date ?? DateTime.Today; 
+
+            Console.WriteLine(date2);
+            Console.WriteLine(date.GetValueOrDefault());
+            Console.WriteLine(date.HasValue);
+
+        }
+    }
+}
